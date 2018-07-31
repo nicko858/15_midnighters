fff95caa609ec62e7d5dbb4b66fa497b0d356f3f
nicko858 <ikar2021@hotmail.com>
2018-07-31 13:20:32 +0300

    Add script and readme

--- README.md
+++ README.md
@@ -1,7 +1,73 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+The script provides data of those, who sent the task for review in period between
+00:00:00 and 04:00:00
+
+# Quickstart
+
+The program is represented by the module seek_dev_nighters.py. Module seek_dev_nighters.py contains the following functions:
+
+- ```get_midnighter_dt()```
+- ```get_pages_count()```
+- ```get_user_local_dt()```
+- ```is_midnighter()```
+- ```load_attempts()```
+- ```print_midnighter_activity()```
+- ```print_title()```
+
+
+The program uses these libs from Python standart and third-party libraries:
+
+```datetime```
+```request```
+```pytz```
+
+**How in works:**
+
+- The program connects to the [devman api](https://devman.org/api/challenges/solution_attempts/)  
+- Reads content page by page
+- Parses every content record 
+- Checks if parsed content(datetime) in 00:00:00 and 04:00:00
+- If true, prints to the console output 
+
+# How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
+Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
+
+
+# How to run
+- Activate virtualenv
+``` bash
+source <path_to_virtualenv>/bin/activate
+```
+- Run script with virtualenv-interpreter
+```bash
+<path_to_virtualenv>/bin/python3.5 seek_dev_nighters.py
+```
+If everything is fine, you'll see such output:
+```text
+Midnighters on devman detected:
+
+User ten.iliya.dm sent the task for review at 2018-07-30 02:42:03.635308+03:00
+User ten.iliya.dm sent the task for review at 2018-07-30 02:25:59.163326+03:00
+User olga.goi sent the task for review at 2018-07-29 00:08:55.316651+03:00
+User olga.goi sent the task for review at 2018-07-29 00:03:23.414893+03:00
+User rtfmagain sent the task for review at 2018-07-27 02:16:44.371999+03:00
+User rtfmagain sent the task for review at 2018-07-27 01:38:23.828371+03:00
+```
+
+In case of [devman api](https://devman.org/api/challenges/solution_attempts/) unavailable, you'll see this message:
+```text
+Site https://devman.org/api/challenges/solution_attempts/ is unavailable!
+```
 
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,90 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+import pytz
+from datetime import datetime
+from pytz import timezone
 
-def get_midnighters():
-    pass
+
+def load_attempts(page_number, api_url):
+    for page in range(1, page_number+1):
+        params = {"page": page}
+        try:
+            response = requests.get(api_url, params=params)
+            if not response.ok:
+                raise ConnectionError
+            response_content = response.json()
+        except (ConnectionError, TimeoutError):
+            return None
+        for record in response_content["records"]:
+            username = record["username"]
+            timestamp = record["timestamp"]
+            timezone = record["timezone"]
+            yield {
+                'username': username,
+                'timestamp': timestamp,
+                'timezone': timezone,
+            }
+
+
+def get_pages_count(api_url):
+    try:
+        response = requests.get(api_url)
+        if not response.ok:
+            raise ConnectionError
+        response_content = response.json()
+    except (ConnectionError, TimeoutError):
+        return None
+    return response_content['number_of_pages']
+
+
+def get_user_local_dt(user_timezone, user_timestamp):
+    utc = pytz.utc
+    utc_dt = utc.localize(datetime.utcfromtimestamp(user_timestamp))
+    user_format_tz = timezone(user_timezone)
+    user_local_dt = utc_dt.astimezone(user_format_tz)
+    return user_local_dt
+
+
+def is_midnighter(user_dt):
+    send_check_time_from = datetime.strptime("00:00:00", "%H:%M:%S")
+    send_check_time_to = datetime.strptime("04:00:00", "%H:%M:%S")
+    if (send_check_time_from.time()
+            <= user_dt.time()
+            <= send_check_time_to.time()):
+        return True
+
+
+def get_midnighter_dt(user_timezone, user_timestamp):
+    user_dt = get_user_local_dt(user_timezone, user_timestamp)
+    if is_midnighter(user_dt):
+        return user_dt
+
+
+def print_title(title_was_printed):
+    if not title_was_printed:
+        print("Midnighters on devman detected:\n")
+
+
+def print_midnighter_activity(username, user_dt):
+    print("User {} sent the task for review at {}".format(
+        username,
+        user_dt
+    ))
+
 
 if __name__ == '__main__':
-  pass
+    api_url = "https://devman.org/api/challenges/solution_attempts/"
+    pages_count = get_pages_count(api_url)
+    if not pages_count:
+        exit("Site {} is unavailable!".format(api_url))
+    title_was_printed = False
+    for attempt in load_attempts(pages_count, api_url):
+        if not attempt:
+            exit("Site {} is unavailable!".format(api_url))
+        user_timestamp = attempt["timestamp"]
+        user_timezone = attempt["timezone"]
+        username = attempt["username"]
+        midnighter_dt = get_midnighter_dt(user_timezone, user_timestamp)
+        if midnighter_dt:
+            print_title(title_was_printed)
+            title_was_printed = True
+            print_midnighter_activity(username, midnighter_dt)
