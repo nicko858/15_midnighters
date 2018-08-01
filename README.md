3583e97f4ac62c9b56f6505af08ef60f5d4a96af
nicko858 <ikar2021@hotmail.com>
2018-08-01 16:00:37 +0300

    - Use while loop iteration through api-pages(replace for loop) - Raise httperror if server respond !=200 - Use fromtimestamp to get user local datetime - Attribute hour helps to compare digits across datetime - Use defaultdict to group midnighters by username - Return bool(expr) across if..true..else..false

--- README.md
+++ README.md
@@ -7,20 +7,17 @@ The script provides data of those, who sent the task for review in period betwee
 
 The program is represented by the module seek_dev_nighters.py. Module seek_dev_nighters.py contains the following functions:
 
-- ```get_midnighter_dt()```
-- ```get_pages_count()```
-- ```get_user_local_dt()```
 - ```is_midnighter()```
 - ```load_attempts()```
 - ```print_midnighter_activity()```
 - ```print_title()```
 
-
 The program uses these libs from Python standart and third-party libraries:
 
-```datetime```
-```request```
-```pytz```
+- ```datetime```
+- ```request```
+- ```pytz```
+- ```defaultdict from collections```
 
 **How in works:**
 
@@ -54,12 +51,12 @@ If everything is fine, you'll see such output:
 ```text
 Midnighters on devman detected:
 
-User ten.iliya.dm sent the task for review at 2018-07-30 02:42:03.635308+03:00
-User ten.iliya.dm sent the task for review at 2018-07-30 02:25:59.163326+03:00
-User olga.goi sent the task for review at 2018-07-29 00:08:55.316651+03:00
-User olga.goi sent the task for review at 2018-07-29 00:03:23.414893+03:00
-User rtfmagain sent the task for review at 2018-07-27 02:16:44.371999+03:00
-User rtfmagain sent the task for review at 2018-07-27 01:38:23.828371+03:00
+User olga.goi sent the tasks for review at: 
+2018-07-29 00:08:55.316651
+2018-07-29 00:03:23.414893
+
+User AndreyArakelyants sent the tasks for review at: 
+2018-07-13 01:16:49
 ```
 
 In case of [devman api](https://devman.org/api/challenges/solution_attempts/) unavailable, you'll see this message:
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,19 +1,28 @@
 import requests
-import pytz
+from requests import HTTPError
 from datetime import datetime
 from pytz import timezone
+from collections import defaultdict
 
 
-def load_attempts(page_number, api_url):
-    for page in range(1, page_number+1):
+def load_attempts(api_url):
+    page = 1
+    first_iteration = True
+    while True:
+        if first_iteration:
+            first_iteration = False
+        else:
+            if page > pages_count:
+                break
         params = {"page": page}
         try:
             response = requests.get(api_url, params=params)
-            if not response.ok:
-                raise ConnectionError
-            response_content = response.json()
-        except (ConnectionError, TimeoutError):
-            return None
+            response.raise_for_status()
+        except (ConnectionError, TimeoutError, HTTPError):
+            yield None
+        response_content = response.json()
+        pages_count = response_content["number_of_pages"]
+        page += 1
         for record in response_content["records"]:
             username = record["username"]
             timestamp = record["timestamp"]
@@ -25,38 +34,12 @@ def load_attempts(page_number, api_url):
             }
 
 
-def get_pages_count(api_url):
-    try:
-        response = requests.get(api_url)
-        if not response.ok:
-            raise ConnectionError
-        response_content = response.json()
-    except (ConnectionError, TimeoutError):
-        return None
-    return response_content['number_of_pages']
-
-
-def get_user_local_dt(user_timezone, user_timestamp):
-    utc = pytz.utc
-    utc_dt = utc.localize(datetime.utcfromtimestamp(user_timestamp))
-    user_format_tz = timezone(user_timezone)
-    user_local_dt = utc_dt.astimezone(user_format_tz)
-    return user_local_dt
-
-
 def is_midnighter(user_dt):
-    send_check_time_from = datetime.strptime("00:00:00", "%H:%M:%S")
-    send_check_time_to = datetime.strptime("04:00:00", "%H:%M:%S")
-    if (send_check_time_from.time()
-            <= user_dt.time()
-            <= send_check_time_to.time()):
-        return True
-
-
-def get_midnighter_dt(user_timezone, user_timestamp):
-    user_dt = get_user_local_dt(user_timezone, user_timestamp)
-    if is_midnighter(user_dt):
-        return user_dt
+    send_check_hour_from = 0
+    send_check_hour_to = 4
+    return (send_check_hour_from
+            <= user_dt.hour
+            <= send_check_hour_to)
 
 
 def print_title(title_was_printed):
@@ -64,27 +47,32 @@ def print_title(title_was_printed):
         print("Midnighters on devman detected:\n")
 
 
-def print_midnighter_activity(username, user_dt):
-    print("User {} sent the task for review at {}".format(
-        username,
-        user_dt
-    ))
+def print_midnighter_activity(midnighter_activity):
+    for username, user_dt in midnighter_activity.items():
+        print("User {} sent the tasks for review at: \n{}\n".format(
+            username,
+            '\n'.join(user_dt)
+        ))
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': 
     api_url = "https://devman.org/api/challenges/solution_attempts/"
-    pages_count = get_pages_count(api_url)
-    if not pages_count:
-        exit("Site {} is unavailable!".format(api_url))
     title_was_printed = False
-    for attempt in load_attempts(pages_count, api_url):
+    midnighters_activity = defaultdict(list)
+    for attempt in load_attempts(api_url):
         if not attempt:
             exit("Site {} is unavailable!".format(api_url))
         user_timestamp = attempt["timestamp"]
         user_timezone = attempt["timezone"]
         username = attempt["username"]
-        midnighter_dt = get_midnighter_dt(user_timezone, user_timestamp)
-        if midnighter_dt:
+        midnighter_dt = datetime.fromtimestamp(
+            user_timestamp,
+            timezone(user_timezone)
+        )
+        if is_midnighter(midnighter_dt):
             print_title(title_was_printed)
             title_was_printed = True
-            print_midnighter_activity(username, midnighter_dt)
+            midnighters_activity[username].append(str(
+                midnighter_dt.replace(tzinfo=None))
+            )
+    print_midnighter_activity(midnighters_activity)
